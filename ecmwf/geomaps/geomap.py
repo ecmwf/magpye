793dadd95ae100ff977058912d808b5a469454ec
James Varndell <james.varndell@ecmwf.int>
2022-01-04 17:54:13 +0000

    Adds more plotting methods with examples

--- ecmwf/geomaps/geomap.py
+++ ecmwf/geomaps/geomap.py
@@ -40,26 +40,41 @@ class GeoMap:
     def register(self, item):
         self.queue.append(item)
 
+    def _map(self, *args, **kwargs):
+        extent = kwargs.pop("extent", None)
+        if extent is not None:
+            (
+                kwargs["lower_left_lat"], kwargs["lower_left_lon"],
+                kwargs["upper_right_lat"], kwargs["upper_right_lon"],
+            ) = extent
+        self.__map(*args, **kwargs)
+
     @action(
         macro.mmap,
         {"subpage_map_area_name": {"subpage_map_library_area": True}},
         area_name="subpage_map_area_name",
         projection="subpage_map_projection",
+        lower_left_lat="subpage_lower_left_latitude",
+        lower_left_lon="subpage_lower_left_longitude",
+        upper_right_lat="subpage_upper_right_latitude",
+        upper_right_lon="subpage_upper_right_longitude",
     )
-    def _map(self, *args, **kwargs):
+    def __map(self, *args, **kwargs):
         pass
 
     @action(
         macro.mcoast,
         {
             "map_rivers": True,
-            "map_coastline": False,
+            "map_coastline": True,
+            "map_coastline_thickness": 0,
             "map_grid": False,
             "map_label": False,
         },
-        colour="map_rivers_colour",
-        style="map_rivers_style",
-        thickness="map_rivers_thickness",
+        resolution="map_coastline_resolution",
+        line_colour="map_rivers_colour",
+        line_style="map_rivers_style",
+        line_thickness="map_rivers_thickness",
     )
     def rivers(self, *args, **kwargs):
         pass
@@ -78,11 +93,11 @@ class GeoMap:
             },
         },
         resolution="map_coastline_resolution",
-        colour="map_coastline_colour",
+        line_colour="map_coastline_colour",
         land_colour="map_coastline_land_shade_colour",
         sea_colour="map_coastline_sea_shade_colour",
-        style="map_coastline_style",
-        thickness="map_coastline_thickness",
+        line_style="map_coastline_style",
+        line_thickness="map_coastline_thickness",
     )
     def coastlines(self, *args, **kwargs):
         pass
@@ -97,9 +112,9 @@ class GeoMap:
         lon_increment="map_grid_longitude_increment",
         lat_reference="map_grid_latitude_reference",
         lon_reference="map_grid_longitude_reference",
-        style="map_grid_line_style",
-        thickness="map_grid_thickness",
-        colour="map_grid_colour",
+        line_style="map_grid_line_style",
+        line_thickness="map_grid_thickness",
+        line_colour="map_grid_colour",
         labels="map_label",
         label_font="map_label_font_style",
         label_colour="map_label_colour",
@@ -111,7 +126,7 @@ class GeoMap:
         label_left_edge="map_label_left",
         label_right_edge="map_label_right",
     )
-    def grid(self, *args, **kwargs):
+    def gridlines(self, *args, **kwargs):
         pass
 
     @action(
@@ -121,19 +136,116 @@ class GeoMap:
     def title(self, text, **kwargs):
         pass
 
-    def data(self, file_name, *args, preset=None, **kwargs):
-        method = {
-            "grib": self._grib,
-            "netcdf": self._netcdf,
-        }[detect_input(file_name)]
+    def _input(self, data):
+        if isinstance(data, str):
+            file_name = data
+            method = {
+                "grib": self._grib,
+                "netcdf": self._netcdf,
+            }[detect_input(file_name)]
+        else:
+            file_name = data.grib_index()[0][0]
+            method = self._grib
         method(file_name)
+
+    def contour_lines(self, data, *args, preset=None, **kwargs):
+        """
+        Plot line contours on a map.
+        """
+        self._input(data)
         self._contour(*args, preset=preset, **kwargs)
+        
+    def shaded_contours(self, data, *args, style=None, preset=None, **kwargs):
+        """
+        Plot filled contours on a map.
+        """
+        self._input(data)
+
+        if isinstance(style, str):
+            kwargs["contour_shade_palette_name"] = style
+
+        self._filled_contour(*args, preset=preset, **kwargs)
 
     @action(
         macro.mcont,
-        style="contour_line_style",
-        colour=["contour_line_colour", "contour_highlight_colour"],
+        {
+            "legend": False,
+            "contour": False,
+            "contour_shade": True,
+            "contour_shade_method": "area_fill",
+            "contour_level_list": {
+                "contour_level_selection_type": "level_list",
+            },
+            "contour_interval": {
+                "contour_level_selection_type": "interval",
+            },
+            "contour_level_list": {
+                "contour_level_selection_type": "level_list",
+            },
+            "contour_label_text": {
+                "contour_label_type": "text",
+            },
+            "contour_shade_palette_name": {
+                "contour_shade_colour_method": "palette",
+            },
+        },
+        levels="contour_level_list",
+        interval="contour_interval",
+        interval_reference="contour_reference_level",
+        dynamic_levels="contour_level_count",
+        fill_pattern="contour_shade_method",
+        dot_size="contour_shade_dot_size",
+        dot_max_density="contour_shade_max_level_density",
+        dot_min_density="contour_shade_min_level_density",
+        hatch_index="contour_shade_hatch_index",
+        hatch_thickness="contour_shade_hatch_thickness",
+        hatch_density="contour_shade_hatch_density",
+        shade_type="contour_shade_technique",
+        contour_method="contour_method",
+    )
+    def _filled_contour(self, *args, **kwargs):
+        pass
+
+    @action(
+        macro.mcont,
+        {
+            "legend": False,
+            "contour": True,
+            "contour_shade": False,
+            "contour_level_list": {
+                "contour_level_selection_type": "level_list",
+            },
+            "contour_interval": {
+                "contour_level_selection_type": "interval",
+            },
+            "contour_level_list": {
+                "contour_level_selection_type": "level_list",
+            },
+            "contour_label_text": {
+                "contour_label_type": "text",
+            }
+        },
+        line_style=["contour_line_style", "contour_highlight_style"],
+        line_colour=["contour_line_colour", "contour_highlight_colour"],
+        line_thickness=["contour_line_thickness", "contour_highlight_thickness"],
+        highlight="contour_highlight",
         highlight_colour="contour_highlight_colour",
+        highlight_style="contour_highlight_style",
+        highlight_thickness="contour_highlight_thickness",
+        highlight_frequency="contour_highlight_frequency",
+        levels="contour_level_list",
+        interval="contour_interval",
+        interval_reference="contour_reference_level",
+        dynamic_levels="contour_level_count",
+        labels="contour_label",
+        label_text="contour_label_text",
+        label_size="contour_label_height",
+        label_blanks="contour_label_blanking",
+        label_quality="contour_label_quality",
+        label_font="contour_label_font",
+        label_font_style="contour_label_font_style",
+        label_colour="contour_label_colour",
+        label_frequency="contour_label_frequency",
     )
     def _contour(self, *args, **kwargs):
         pass
@@ -147,7 +259,10 @@ class GeoMap:
 
     @action(
         macro.mnetcdf,
-        file_name="netcdf_file_name",
+        {
+            "netcdf_value_variable": "psl",
+        },
+        file_name="netcdf_filename",
     )
     def _netcdf(self, *args, **kwargs):
         pass
