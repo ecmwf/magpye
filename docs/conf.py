fe531f024c9587a177028ed002a559dbd162b06b
Sylvie Lamy-Thepaut <sylvie.lamy-thepaut@ecmwf.int>
2022-01-23 17:56:23 +0000

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -70,7 +70,6 @@ extensions = [
     "IPython.sphinxext.ipython_directive",
     "IPython.sphinxext.ipython_console_highlighting",
     "sphinx.ext.graphviz",
-    "sphinx_gallery.gen_gallery"
     # "sphinx-prompt",
     # "climetlab.sphinxext.sources",
     # "climetlab.sphinxext.command_output",
